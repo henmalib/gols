dee3f59d4d5919745af1aa1720635d7598a731e9
simidzu2ay <simidzu2ay@protonmail.com>
2025-01-14 19:05:59 +0100

    init: basic web-server

---
+++ packages/web/server.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-playground/validator/v10"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Url struct {
+	Id    int
+	Url   string
+	Short string
+
+	CreatedAt time.Time
+}
+
+func WriteError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
+type CreateLinkPayload struct {
+	Url   string `json:"url" validate:"required,uri"`
+	Short string `json:"short" validate:"required"`
+}
+
+var validate = validator.New(validator.WithRequiredStructEnabled())
+
+func EnsureDB() (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", "file:./database/db.sqlite?cache=shared&mode=rwc")
+
+	if err != nil {
+		return db, err
+	}
+	db.SetMaxOpenConns(1)
+
+	if err = db.Ping(); err != nil {
+		return db, err
+	}
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS urls (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            short TEXT UNIQUE NOT NULL,
+            original TEXT NOT NULL,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+        );`)
+
+	if err != nil {
+		return db, err
+	}
+
+	return db, nil
+}
+
+func getEnv() (string, string, error) {
+	host := os.Getenv("HOST")
+	apiKey := os.Getenv("API_KEY")
+
+	if host == "" {
+		host = ":5050"
+	}
+	if apiKey == "" {
+		return host, apiKey, errors.New("No API_KEY provided")
+	}
+
+	return host, apiKey, nil
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	host, apiKey, err := getEnv()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	db, err := EnsureDB()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// TODO: check for auth key
+	mux.HandleFunc("POST /api/links", func(w http.ResponseWriter, r *http.Request) {
+		authKey := r.Header.Get("Authorization")
+		if authKey != apiKey {
+			WriteError(w, http.StatusUnauthorized)
+			return
+		}
+
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			WriteError(w, http.StatusInternalServerError)
+		}
+
+		payload := CreateLinkPayload{}
+
+		if err = json.Unmarshal(body, &payload); err != nil {
+			WriteError(w, http.StatusBadRequest)
+			return
+		}
+
+		if err = validate.Struct(payload); err != nil {
+			WriteError(w, http.StatusBadRequest)
+			return
+		}
+
+		_, err = db.Exec(`INSERT INTO urls (short, original) VALUES (?, ?);`, payload.Short, payload.Url)
+		if err != nil {
+			http.Error(w, "Short link with this url already exists", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Fprintf(w, "http://localhost:5050/%s", payload.Short)
+	})
+
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		row := db.QueryRow("SELECT original FROM urls WHERE short = ?", r.URL.String())
+		url := &Url{}
+
+		err := row.Scan(&url.Url)
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				WriteError(w, http.StatusNotFound)
+				return
+			}
+
+			log.Println(err)
+			WriteError(w, http.StatusBadRequest)
+			return
+		}
+
+		http.Redirect(w, r, url.Url, http.StatusMovedPermanently)
+	})
+
+	server := http.Server{
+		Addr:    host,
+		Handler: mux,
+	}
+
+	log.Printf("Server is starting: %s", host)
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalln(err)
+	}
+}
